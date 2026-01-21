6ec01ed3edf8561ed35acee3bc6f75fa40052c1f
PrzemyslawKepka <przemyslaw.kepka@yahoo.com>
2026-01-21 15:03:47 +0700

    changing used logo pictures

--- src/utils/companyLogos.ts
+++ src/utils/companyLogos.ts
@@ -1,8 +1,8 @@
 // Mapping of company names to their logo file paths
 const companyLogos: Record<string, string> = {
-  'Santander Bank Poland': '/images/icons/companies/Santander_idE2Zv_6nc_logos.svg',
-  'Ascensia Diabetes Care': '/images/icons/companies/ascensia_diabetes_care_logo.webp',
-  'JLL': '/images/icons/companies/JLL_idwD2CH2ZL_1768978037301.svg',
+  'Santander Bank Poland': '/images/icons/companies/Santander_id_cIxIG4D_logos.jpeg',
+  'Ascensia Diabetes Care': '/images/icons/companies/Ascensia_idL44xPnly_logos.jpeg',
+  'JLL': '/images/icons/companies/JLL_Symbol_0.svg',
 };
 
 export function getCompanyLogo(companyName: string | undefined): string | null {
